8d769b40b1d948efc64a3b74371fbd31caa9d966
Mavis Ou <mmmavis@users.noreply.github.com>
2018-09-24 13:01:15 -0700

    added functionality to generate data and files for Guidebook session import

---
+++ src/scripts/lib/export-as-json.js
@@ -0,0 +1,12 @@
+import jsonfile from 'jsonfile';
+import chalk from 'chalk';
+
+export default function(fileContent, filePath, cb) {
+  jsonfile.writeFile(filePath, fileContent, {spaces: 2}, (err) => {
+    if (err) {
+      cb(err, filePath);
+    }
+    console.log(chalk.blue(`[JSON File Saved] ${filePath}`));
+    cb();
+  });
+}
---
+++ src/scripts/lib/get-sessions-from-spreadsheet.js
@@ -0,0 +1,34 @@
+import GoogleSpreadsheet from 'google-spreadsheet';
+import getEnvVars from './get-env-vars';
+
+const ENV_VARS = getEnvVars();
+const GOOGLE_API_CLIENT_EMAIL_2018 = ENV_VARS.GOOGLE_API_CLIENT_EMAIL_2018;
+const GOOGLE_API_PRIVATE_KEY_2018 = ENV_VARS.GOOGLE_API_PRIVATE_KEY_2018;
+const ACCEPTED_PROPOSALS_GOOGLE_SPREADSHEET_ID = ENV_VARS.ACCEPTED_PROPOSALS_GOOGLE_SPREADSHEET_ID;
+
+export default function(callback) {
+  var sheet = new GoogleSpreadsheet(ACCEPTED_PROPOSALS_GOOGLE_SPREADSHEET_ID);
+
+  // line breaks are essential for the private key.
+  // if reading this private key from env var this extra replace step is a MUST
+  sheet.useServiceAccountAuth({
+    "client_email": GOOGLE_API_CLIENT_EMAIL_2018,
+    "private_key": GOOGLE_API_PRIVATE_KEY_2018.replace(/\\n/g, `\n`)
+  }, (err) => {
+    if (err) {
+      console.log(`[Error] ${err}`);
+      callback(err);
+    }
+
+    // GoogleSpreadsheet.getRows(worksheet_id, callback)
+    // worksheet_id - the index of the sheet to read from (index starts at 1)
+    sheet.getRows(1, (getRowError, rows) => {
+      console.log(rows.length);
+      if (getRowError) {
+        console.log(`[getRowError]`, getRowError);
+      }
+
+      callback(err, rows);
+    });
+  });
+}
